1f0f102e897e733fbf0aa12dd76cc0526e1b6525
sacha-froment-ext <sfroment42@gmail.com>
2020-08-03 18:10:40 +0200

    go.mod
    Signed-off-by: sacha-froment-ext <sfroment42@gmail.com>

--- checkmail_test.go
+++ checkmail_test.go
@@ -4,31 +4,28 @@ import (
 	"fmt"
 	"os"
 	"testing"
-
 )
 
-var (
-	samples = []struct {
-		mail    string
-		format  bool
-		account bool //host+user
-	}{
-		{mail: "florian@carrere.cc", format: true, account: true},
-		{mail: "support@g2mail.com", format: true, account: false},
-		{mail: " florian@carrere.cc", format: false, account: false},
-		{mail: "florian@carrere.cc ", format: false, account: false},
-		{mail: "test@912-wrong-domain902.com", format: true, account: false},
-		{mail: "0932910-qsdcqozuioqkdmqpeidj8793@gmail.com", format: true, account: false},
-		{mail: "@gmail.com", format: false, account: false},
-		{mail: "test@gmail@gmail.com", format: false, account: false},
-		{mail: "test test@gmail.com", format: false, account: false},
-		{mail: " test@gmail.com", format: false, account: false},
-		{mail: "test@wrong domain.com", format: false, account: false},
-		{mail: "é&ààà@gmail.com", format: false, account: false},
-		{mail: "admin@busyboo.com", format: true, account: false},
-		{mail: "a@gmail.fi", format: true, account: false},
-	}
-)
+var samples = []struct {
+	mail    string
+	format  bool
+	account bool // host+user
+}{
+	{mail: "florian@carrere.cc", format: true, account: true},
+	{mail: "support@g2mail.com", format: true, account: false},
+	{mail: " florian@carrere.cc", format: false, account: false},
+	{mail: "florian@carrere.cc ", format: false, account: false},
+	{mail: "test@912-wrong-domain902.com", format: true, account: false},
+	{mail: "0932910-qsdcqozuioqkdmqpeidj8793@gmail.com", format: true, account: false},
+	{mail: "@gmail.com", format: false, account: false},
+	{mail: "test@gmail@gmail.com", format: false, account: false},
+	{mail: "test test@gmail.com", format: false, account: false},
+	{mail: " test@gmail.com", format: false, account: false},
+	{mail: "test@wrong domain.com", format: false, account: false},
+	{mail: "é&ààà@gmail.com", format: false, account: false},
+	{mail: "admin@busyboo.com", format: true, account: false},
+	{mail: "a@gmail.fi", format: true, account: false},
+}
 
 func TestValidateHost(t *testing.T) {
 	for _, s := range samples {
@@ -64,7 +61,7 @@ func TestValidateMX(t *testing.T) {
 
 func TestValidateHostAndUser(t *testing.T) {
 	var (
-		serverHostName = getenv(t, "self_hostname")
+		serverHostName    = getenv(t, "self_hostname")
 		serverMailAddress = getenv(t, "self_mail")
 	)
 	for _, s := range samples {
@@ -95,8 +92,8 @@ func TestValidateFormat(t *testing.T) {
 }
 
 func getenv(t *testing.T, name string) (value string) {
-	name = "test_checkmail_"+name
-	if value = os.Getenv(name); value =="" {
+	name = "test_checkmail_" + name
+	if value = os.Getenv(name); value == "" {
 		panic(fmt.Errorf("enviroment variable %q is not defined", name))
 	}
 	return
